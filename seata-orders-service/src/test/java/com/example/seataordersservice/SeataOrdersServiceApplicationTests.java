7164465ccfc60b21c955a61088db9bd5bd7bd974
TW - IT - Ray Lin <ray.lin@shoalter.com>
2022-02-25 14:21:39 +0800

    refactor: remove empty unit test

--- seata-orders-service/src/test/java/com/example/seataordersservice/SeataOrdersServiceApplicationTests.java
+++ seata-orders-service/src/test/java/com/example/seataordersservice/SeataOrdersServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.seataordersservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SeataOrdersServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
--- seata-stock-service/src/test/java/com/example/seatastockservice/SeataStockServiceApplicationTests.java
+++ seata-stock-service/src/test/java/com/example/seatastockservice/SeataStockServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.seatastockservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SeataStockServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
